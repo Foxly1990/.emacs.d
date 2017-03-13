0c907ba1da9230c9c0c9df7ddf20b35c35d79ab3
Foxly1990 <flokistable@protonmail.com>
2017-03-13 13:17:32 +0300

    Renew my .emacs.d

--- elpa/bs-ext-20130824.459/bs-ext-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "bs-ext" "20130824.459" "Extensions to emacs buffer-selection library (bs.el)" 'nil :url "http://www.emacswiki.org/emacs/download/bs-ext.el" :keywords '("convenience" "files"))
--- init.el
+++ init.el
@@ -1,111 +1,74 @@
+;;; init.el --- init file
+;;; Commentary:
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
+;;; Code:
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
+;; Установка отсутствующих пакетов
+;; Список пакетов, которые участвуют в системе
+(defvar package-list '(ergoiemacs-mode
+                       cider
+                       flycheck
+                       js2-mode
+                       js2-refactor
+                       tern
+                       tern-auto-complete
+                       pdf-tools
+                       yasnippet
+                       autopair
+                       web-mode
+                       chess
+                       jade-mode
+                       emmet-mode
+                       smex
+                       helm
+                       elfeed
+                       magit
+                       auto-complete))
+
+;; Подключаем репозитории
+(defvar package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                           ("melpa" . "http://melpa.milkbox.net/packages/")
+                           ("marmalade" . "http://marmalade-repo.org/packages/")))
+
+;; Иницилизуем пакеты
 (package-initialize)
 
-;; Сообщение в scratch при старте emacs
-;;(setq initial-scratch-message
-;;(propertize "Привет, Руслан! Я вижу ты зашёл в свой любимый редактор?\nНе хочешь немного позаниматься в программированием?"
-;;'font-lock-face '(:height 10.0 :inherit variable-pitch))
-;;inhibit-startup-screen t)
+;; Поверяем наличие установленные пакеты
+(unless package-archive-contents
+  (package-refresh-contents))
 
-(add-to-list 'load-path "~/.emacs.d/floki")
-(require 'fmain)
-(require 'ffunctions)
-(require 'fincludes)
-(require 'fbinds)
+;; Перебираем наш список и из него доустанавливаем пакеты
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+;; Загрузка файлов конфигурации модов
+(defun load-directory (dir)
+  "DIR путь до директории, которую нужно перебрать.
+Функция перебирает все файлы с расширением el и подгружает их."
+  (let ((load-it (lambda (f)
+                   (load-file (concat (file-name-as-directory dir) f)))
+                 ))
+    (mapc load-it (directory-files dir nil "\\.el$"))))
+
+;; Вызываем функцию...
+(load-directory "~/.emacs.d/config")
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#ded6c5" "#f71010" "#028902" "#ef8300" "#0000ff" "#a020f0" "#358d8d" "#262626"])
- '(custom-enabled-themes (quote (fogus)))
- '(custom-safe-themes
-   (quote
-    ("bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "7153b82e50b6f7452b4519097f880d968a6eaf6f6ef38cc45a144958e553fbc6" "3d5ef3d7ed58c9ad321f05360ad8a6b24585b9c49abcee67bdcbb0fe583a6950" default)))
- '(diary-entry-marker (quote font-lock-variable-name-face))
- '(emms-mode-line-icon-image-cache
-   (quote
-    (image :type xpm :ascent center :data "/* XPM */
-static char *note[] = {
-/* width height num_colors chars_per_pixel */
-\"    10   11        2            1\",
-/* colors */
-\". c #358d8d\",
-\"# c None s None\",
-/* pixels */
-\"###...####\",
-\"###.#...##\",
-\"###.###...\",
-\"###.#####.\",
-\"###.#####.\",
-\"#...#####.\",
-\"....#####.\",
-\"#..######.\",
-\"#######...\",
-\"######....\",
-\"#######..#\" };")))
- '(fci-rule-color "#f6f0e1")
- '(gnus-logo-colors (quote ("#0d7b72" "#adadad")))
- '(gnus-mode-line-image-cache
-   (quote
-    (image :type xpm :ascent center :data "/* XPM */
-static char *gnus-pointer[] = {
-/* width height num_colors chars_per_pixel */
-\"    18    13        2            1\",
-/* colors */
-\". c #358d8d\",
-\"# c None s None\",
-/* pixels */
-\"##################\",
-\"######..##..######\",
-\"#####........#####\",
-\"#.##.##..##...####\",
-\"#...####.###...##.\",
-\"#..###.######.....\",
-\"#####.########...#\",
-\"###########.######\",
-\"####.###.#..######\",
-\"######..###.######\",
-\"###....####.######\",
-\"###..######.######\",
-\"###########.######\" };")))
- '(package-selected-packages
-   (quote
-    (typescript-mode yasnippet web-mode sublime-themes spacemacs-theme smex slime persistent-soft nyan-mode nlinum magit linum-relative grandshell-theme evil-visual-mark-mode ergoemacs-mode emmet-mode color-theme-sanityinc-tomorrow clojure-mode bs-ext avy autumn-light-theme autopair auto-complete alect-themes ace-jump-mode ac-js2 abyss-theme)))
- '(vc-annotate-background "#f6f0e1")
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#e43838")
-     (40 . "#f71010")
-     (60 . "#ab9c3a")
-     (80 . "#9ca30b")
-     (100 . "#ef8300")
-     (120 . "#958323")
-     (140 . "#1c9e28")
-     (160 . "#3cb368")
-     (180 . "#028902")
-     (200 . "#008b45")
-     (220 . "#077707")
-     (240 . "#259ea2")
-     (260 . "#358d8d")
-     (280 . "#0eaeae")
-     (300 . "#2c53ca")
-     (320 . "#0000ff")
-     (340 . "#0505cc")
-     (360 . "#a020f0"))))
- '(vc-annotate-very-old-color "#a020f0"))
+ '(package-selected-packages (quote (pdf-tools paperless persistent-soft)))
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "DejaVu Sans Mono" :foundry "PfEd" :slant normal :weight normal :height 98 :width normal)))))
+
+;;; init.el ends here
